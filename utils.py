92e7418231b9cc2e9496de6dcf8eb645b37ad7b9
AdrianD <Adrian.Domagala71@gmail.com>
2025-09-04 21:25:46 +0200

    spoko w chuj 2

--- utils.py
+++ utils.py
@@ -1,9 +1,26 @@
+import logging
 import os
+import sqlite3
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
 def delete_data_files():
+    files_deleted = []
     for filename in os.listdir(current_dir):
         if filename.endswith(".tmp"):
+            files_deleted.append(filename)
             os.remove(os.path.join(current_dir, filename))
+
+    logging.info(f"Usunieto plikow {len(files_deleted)} i takie np: {files_deleted}")
+
+
+
+raporty_db = "raporty_finansowe.db"
+
+
+def get_raport_db_connection():
+    conn = sqlite3.connect(raporty_db)
+    conn.row_factory = sqlite3.Row
+    return conn
+
